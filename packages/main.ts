a69ec6df0c5ae23df33ff55b8f7e69f046e52d31
flowwishthebest <togowiththeflow@gmail.com>
2020-02-13 17:01:04 +0300

    Fix ci

--- packages/main.ts
+++ packages/main.ts
@@ -1,9 +1,8 @@
 import { Parser } from './parser';
-import { Tokenizer } from "./tokenizer";
+import { Tokenizer } from './tokenizer';
 
 function main() {
     const parser = new Parser(new Tokenizer('1 + 2 + 3 + 4'));
-    debugger;
     parser.parse();
 }
 
