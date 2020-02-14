5b3c425297ab6b09abb7f94a029e67e6824dcd78
Ilya P <32912855+flowwishthebest@users.noreply.github.com>
2020-02-14 17:08:13 +0300

    Added multiple operands&ops handler (#10)
    * wip
    
    * Added multiple operators handler
    
    * Fix tests
    
    * Added assert
    
    * Fix tests

--- __tests__/parser.spec.ts
+++ __tests__/parser.spec.ts
@@ -1,40 +1,71 @@
+/**
+ * test(description, function testFunction() {
+ *  // arrange
+ *
+ *  // act
+ *
+ *  // assert
+ *
+ * });
+ */
+
 import { Parser } from '../packages/parser';
 import { Tokenizer } from '../packages/tokenizer';
 
-test('Add op', () => {
-    const parser = new Parser(new Tokenizer('1 + 2'));
+describe('Parser tests', () => {
 
-    expect(parser.parse()).toEqual(3);
-});
+    test('Add op', () => {
+        const parser = new Parser(new Tokenizer('1 + 2'));
 
-test('Subtract op', () => {
-    const parser = new Parser(new Tokenizer('1 - 2'));
+        const result = parser.parse();
 
-    expect(parser.parse()).toEqual(-1);
-});
+        expect(result).toEqual(3);
+    });
 
-test('Unsupported op throw error', () => {
-    const parser = new Parser(new Tokenizer('1 & 2'));
+    test('Subtract op', () => {
+        const parser = new Parser(new Tokenizer('1 - 2'));
 
-    let thrownError;
-    try {
-        parser.parse();
-    } catch (err) {
-        thrownError = err;
-    }
+        const result = parser.parse();
 
-    expect(thrownError).toBeDefined();
-    expect(thrownError.message).toEqual('Unsupported token type &');
-});
+        expect(result).toEqual(-1);
+    });
 
-test('Can handle much operands', () => {
-    const parser = new Parser(new Tokenizer('1 + 2 + 3 + 4'));
+    test('Unsupported op throw error', () => {
+        const parser = new Parser(new Tokenizer('1 & 2'));
 
-    expect(parser.parse()).toEqual(10);
-});
+        let thrownError;
+        try {
+            parser.parse();
+        } catch (err) {
+            thrownError = err;
+        }
 
-test('Can handle much operands', () => {
-    const parser = new Parser(new Tokenizer('1 + 2 + 3 + 4'));
+        expect(thrownError).toBeDefined();
+        expect(thrownError.message).toEqual('Unsupported token type &');
+    });
 
-    expect(parser.parse()).toEqual(10);
+    test('Can handle much operands', () => {
+        const parser = new Parser(new Tokenizer('1 + 2 + 3 + 4'));
+
+        const result = parser.parse();
+
+        expect(result).toEqual(10);
+    });
+
+    test('Can handle much operands', () => {
+        const parser = new Parser(new Tokenizer('1 + 2 * 3 + 4'));
+
+        const result = parser.parse();
+
+        expect(result).toEqual(11);
+    });
+
+    test('Can handle much operands', () => {
+        const parser = new Parser(new Tokenizer('1 + 2 * 20 + 4 / 4'));
+
+        const result = parser.parse();
+
+        expect(result).toEqual(42);
+    });
+
 });
--- packages/parser.ts
+++ packages/parser.ts
@@ -1,8 +1,12 @@
+import { equal } from 'assert';
 import { Tokenizer } from './tokenizer';
+import { DivToken } from './tokens/div.token';
 import { MinusToken } from './tokens/minus.token';
+import { MulToken } from './tokens/mul.token';
 import { NumberToken } from './tokens/number.token';
 import { PlusToken } from './tokens/plus.token';
 import { Token } from './tokens/token';
+import { ETokenType } from './types/token.type';
 
 export class Parser {
     // @Parser = [token, ..., token] -> ast
@@ -17,9 +21,7 @@ export class Parser {
 
     public parse(): any {
 
-        let result = this._currentToken.getValue();
-
-        this._currentToken = this._tokenizer.getNextToken();
+        let result = this._term();
 
         while (this._currentToken instanceof MinusToken
             || this._currentToken instanceof PlusToken
@@ -30,15 +32,50 @@ export class Parser {
                 this._currentToken = this._tokenizer.getNextToken();
 
                 if (this._currentToken instanceof NumberToken) {
-                    result += this._currentToken.getValue();
+                    result += this._term();
                 }
-
-                this._currentToken = this._tokenizer.getNextToken();
             } else if (op instanceof MinusToken) {
                 this._currentToken = this._tokenizer.getNextToken();
 
                 if (this._currentToken instanceof NumberToken) {
-                    result -= this._currentToken.getValue();
+                    result -= this._term();
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private _factor(): number {
+        equal(this._currentToken.getType(), ETokenType.NUMBER);
+        return this._currentToken.getValue();
+    }
+
+    private _term(): number {
+        equal(this._currentToken.getType(), ETokenType.NUMBER);
+
+        let result = this._currentToken.getValue();
+
+        this._currentToken = this._tokenizer.getNextToken();
+
+        while (this._currentToken instanceof DivToken
+            || this._currentToken instanceof MulToken
+        ) {
+            const op = this._currentToken;
+
+            if (op instanceof DivToken) {
+                this._currentToken = this._tokenizer.getNextToken();
+
+                if (this._currentToken instanceof NumberToken) {
+                    result /= this._currentToken.getValue();
+                }
+
+                this._currentToken = this._tokenizer.getNextToken();
+            } else if (op instanceof MulToken) {
+                this._currentToken = this._tokenizer.getNextToken();
+
+                if (this._currentToken instanceof NumberToken) {
+                    result *= this._currentToken.getValue();
                 }
 
                 this._currentToken = this._tokenizer.getNextToken();
