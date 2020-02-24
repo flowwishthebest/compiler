7b77633fc3ce8d22d47d618e397c6765d72b357a
Ilya P <32912855+flowwishthebest@users.noreply.github.com>
2020-02-24 17:47:49 +0300

    Added semantic analyzer (#18)
    * Added procedure declaration
    
    * Added semantic analyzer

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,20 @@
 import { Parser } from './parser';
 import { Tokenizer } from './tokenizer';
 import { Interpreter } from './interpreter';
-import { SymbolTableBuilder } from './symbol-table-builder';
+import { SemanticAnalyzer } from './semantic-analyzer';
 
 (function main(): any {
     const tokenizer = new Tokenizer(`
-        program Feature11;
-        var a: integer;
-    
-        procedure p1;
-        var a: float; k: integer;
-    
-        procedure p2;
-        var a, z: integer;
-        { // p2
-            z := 777;
-        }; // end p2
-        
-        { // p1
-            
-        }; // end p1
-        
-        { // Feature11
-            a := 10;
-        } // Feature1
-    `);
+        program SymTab5;
+        var x : integer; x : float; { x := x; }`);
 
-    const symbolTableBuilder = new SymbolTableBuilder();
+    const semanticAnalyzer = new SemanticAnalyzer();
     const parser = new Parser(tokenizer);
     const tree = parser.parse();
     const interpreter = new Interpreter(tree);
 
     interpreter.interpret();
-    symbolTableBuilder.visit(tree);
+    semanticAnalyzer.visit(tree);
 
     console.log('GLOBAL_SCOPE', interpreter.getGlobalScope());
 })();
