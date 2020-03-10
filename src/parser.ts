31344f9b3737e2b6de6c6b0d8da77cfba0a946a4
flowwishthebest <togowiththeflow@gmail.com>
2020-03-10 20:00:20 +0300

    Upd parser

--- src/parser.ts
+++ src/parser.ts
@@ -510,6 +510,102 @@ export class Parser {
     }
 
     private __comparition__(): any {
-        return ;
+        let expr = this.__addition__();
+
+        const op = this._currentToken;
+
+        while (op.getType() === ETokenType.GREATER ||
+            op.getType() === ETokenType.GREATER_EQUAL ||
+            op.getType() === ETokenType.LESS ||
+            op.getType() === ETokenType.LESS_EQUAL
+        ) {
+            const right = this.__addition__();
+
+            expr = new BinOpAST(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private __addition__(): any {
+        let expr = this.__multiplication__();
+
+        const op = this._currentToken;
+
+        while (
+            op.getType() === ETokenType.MINUS ||
+            op.getType() === ETokenType.PLUS
+        ) {
+            const right = this.__multiplication__();
+
+            expr = new BinOpAST(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private __multiplication__(): any {
+        let expr = this.__unary__();
+
+        const op = this._currentToken;
+
+        while (op.getType() === ETokenType.FLOAT_DIV ||
+            op .getType() === ETokenType.MUL
+        ) {
+            const right = this.__unary__();
+
+            expr = new BinOpAST(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private __unary__(): any {
+        if (this._currentToken.getType() === ETokenType.BANG ||
+            this._currentToken.getType() === ETokenType.MINUS
+        ) {
+            const op = this._currentToken;
+
+            const right = this.__unary__();
+
+            return new UnaryOpAST(op, right);
+        }
+
+        return this.__primary__();
+    }
+
+    private __primary__(): any {
+        switch (this._currentToken.getType()) {
+            case ETokenType.TRUE:
+            case ETokenType.FALSE:
+            case ETokenType.NIL: {
+                return new LiteralAST(this._currentToken);
+            }
+
+            case ETokenType.INTEGER_CONST:
+            case ETokenType.FLOAT_CONST: {
+                return new LiteralAST(this._currentToken);
+            }
+        }
+    }
+
+    private __match__(...types: Array<string>): boolean {
+        for (const t of types) {
+            if (this.__check__(t)) {
+
+            }
+        }
+    }
+
+    private __advance__(): void {
+        this._currentToken = this._tokenizer.getNextToken();
+    }
+
+    private __peek__(): Token {
+        return this._currentToken;
+    }
+
+    private __check__(type: string): boolean {
+        return this.__peek__().getType() === type;
     }
 }
